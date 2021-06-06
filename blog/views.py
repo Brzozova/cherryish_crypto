ae298f48ac96d6740ac84a0fcd3d7f3cf822d1eb
Brzozova <brzozowwwska@gmail.com>
2021-06-06 23:55:31 +0200

    Add first urls and basic HTML.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.conf.urls import include
 
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
 # Create your views here.
